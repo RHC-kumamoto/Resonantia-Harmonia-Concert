308a164b65262c1e984057acb0f1bcc67bcd8e8e
RHC-kumamoto <smhznnmv2z@privaterelay.appleid.com>
2026-09-05 21:58:29 +0900

    Add link to committee introduction in navigation

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <nav>
             <a href="index.html">HOME</a>
             <a href="rhc.html">RHCとは</a>
+            <a href="committee.html">実行委員会紹介</a>
             <a href="documents.html">各種書類ダウンロード</a>
             <a href="activities.html">活動の様子</a>
         </nav>
